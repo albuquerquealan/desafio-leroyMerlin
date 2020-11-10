ec0cddbb095896f3a0f443371abe0eb6ec951e2f
albuquerqueAlan <alan.nicolas@outlook.com>
2020-11-10 00:05:58 -0300

    Classe inicializadora

---
+++ src/main/java/br/com/leroymerlin/test/BackendTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.leroymerlin.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/leroymerlin/test/BackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leroymerlin.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
